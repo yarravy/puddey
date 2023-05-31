a1611e14fe468551bafc75bef9b23d84d9daa65d
Yarravy <ytanh.nguyen@gmail.com>
2023-05-31 23:32:35 +1000

    create home screen for task to do and other task headings

---
+++ script.js
@@ -0,0 +1,49 @@
+function openInbox() {
+    document.getElementById("myInbox").style.display="flex";
+    document.getElementById("myToday").style.display="none";
+    document.getElementById("myWeek").style.display="none";
+    document.getElementById("myCompleted").style.display="none";
+    document.getElementById("myTrash").style.display="none";
+}
+
+function openToday() {
+    document.getElementById("myToday").style.display="flex";
+    document.getElementById("myInbox").style.display="none";
+    document.getElementById("myWeek").style.display="none";
+    document.getElementById("myCompleted").style.display="none";
+    document.getElementById("myTrash").style.display="none";
+}
+
+function openWeek() {
+    document.getElementById("myWeek").style.display="flex";
+    document.getElementById("myInbox").style.display="none";
+    document.getElementById("myToday").style.display="none";
+    document.getElementById("myCompleted").style.display="none";
+    document.getElementById("myTrash").style.display="none";
+}
+
+function openCompleted() {
+    document.getElementById("myCompleted").style.display="flex";
+    document.getElementById("myWeek").style.display="none";
+    document.getElementById("myInbox").style.display="none";
+    document.getElementById("myToday").style.display="none";
+    document.getElementById("myTrash").style.display="none";
+}
+
+function openTrash() {
+    document.getElementById("myWeek").style.display="none";
+    document.getElementById("myInbox").style.display="none";
+    document.getElementById("myToday").style.display="none";
+    document.getElementById("myCompleted").style.display="none";
+    document.getElementById("myTrash").style.display="flex";
+}
+
+function openTaskForm() {
+    document.getElementById("taskAdd").style.display="flex";
+}
+
+function closeTaskForm() {
+    document.getElementById("taskAdd").style.display="none";
+    document.getElementById("taskName").required=false;
+    document.getElementById("taskDescription").required=false;
+}
