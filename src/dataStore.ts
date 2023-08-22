5e151ede3498c7f7d303f8960125b7d939d70422
Yarravy <ytanh.nguyen@gmail.com>
2023-08-22 12:20:26 +1000

    create user type in dataStore

---
+++ src/dataStore.ts
@@ -0,0 +1,13 @@
+export type data = {
+    users: user[],
+}
+
+export type user = {
+    firstName: string,
+    lastName: string,
+    DOB: Date,
+    email: string,
+    password: string,
+    username: string,
+    userID: number,
+} 
